9e06ac1721a7ee98c2d04c00bdbf041a27e9b09a
Pitchoune <webmaster@zelda-series.net>
2022-05-03 09:03:43 +0200

    chore(setup): install symfony files

---
+++ src/Controller/TestController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController extends AbstractController
+{
+    /**
+     * @Route("/test", name="app_test")
+     */
+    public function index(): Response
+    {
+        $nomRoute = "je suis sur la route test";
+        return $this->render('test/index.html.twig', [
+            'controller_name' => 'Page de TestController',
+            'route' => $nomRoute
+        ]);
+    }
+}
